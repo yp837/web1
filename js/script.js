d37e9049455f8e0cdb02bf84e4958cc3f7da0d03
yp837 <ypzheng837@gmail.com>
2024-09-01 15:29:16 -0400

    update multi pages

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//assignment 1 page
+//using the queryselector method to search class matching in the DOM
+window.addEventListener('load',function(){
+    document.querySelector('.assignmentpic1').classList.add('visible')
+});
+
+
